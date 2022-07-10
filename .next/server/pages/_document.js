c200975676aec1d4c91080bd147e404f738cbf6c
stevenmcsorley <smcsorl@gmail.com>
2022-07-10 20:53:29 +0100

    fix about

--- .next/server/pages/_document.js
+++ .next/server/pages/_document.js
@@ -124,7 +124,7 @@ module.exports = require("react/jsx-runtime");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [730,859], () => (__webpack_exec__(208)));
+var __webpack_exports__ = __webpack_require__.X(0, [859], () => (__webpack_exec__(208)));
 module.exports = __webpack_exports__;
 
 })();
